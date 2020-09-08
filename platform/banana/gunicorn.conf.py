9e1daef9ac74010cf115acd9d0a75fb503c7f7aa
ArcLight_Slavik <arclight.leskiv@gmail.com>
2020-09-08 15:16:58 +0300

    Don't have a default port selection

--- platform/banana/gunicorn.conf.py
+++ platform/banana/gunicorn.conf.py
@@ -1,13 +1,8 @@
-# import multiprocessing
 import os
 
-host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", "8000")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
 
-# Gunicorn config variables
-# workers = multiprocessing.cpu_count() * 2 + 1
-
-# For debugging and testing
 log_data = {
     "host": host,
     "port": port,
