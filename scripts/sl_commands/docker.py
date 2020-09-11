e6170fd8a1b0dff94ac1933217512069e52463fa
ArcLight_Slavik <arclight.leskiv@gmail.com>
2020-09-11 10:39:21 +0300

    Change the default value for no dependencies

--- scripts/sl_commands/docker.py
+++ scripts/sl_commands/docker.py
@@ -1,7 +1,7 @@
 from . import utils
 
 
-def run(docker_image, cmd, no_deps=False):
+def run(docker_image, cmd, no_deps=True):
     # If recruiter_{service}_1 is not running, runs a temp container, execute command and remove container.
     # If container is already running, "exec" inside container
 
