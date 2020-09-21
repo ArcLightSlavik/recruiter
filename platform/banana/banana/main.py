83fb5a49af237a57d336eeceb7cdd68b7b4a828f
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-09-22 01:14:42 +0300

    Simple postgres setup

--- platform/banana/banana/main.py
+++ platform/banana/banana/main.py
@@ -1,8 +1,45 @@
 import fastapi
+import pydantic
 
-from typing import Dict
+from typing import Dict, List
+
+from .postgres_db import database, notes
 
 app = fastapi.FastAPI()
+
+
+class NoteIn(pydantic.BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(pydantic.BaseModel):
+    id: int
+    text: str
+    completed: bool
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
 
 
 @app.get("/")
