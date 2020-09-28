8311d5a0ccc401d0a6a67fc94909e8310314e154
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-09-28 12:45:09 +0300

    Postgres tortoise settings

--- platform/banana/banana/postgres_db.py
+++ platform/banana/banana/postgres_db.py
@@ -1,11 +1,27 @@
-import os
-import databases
-import sqlalchemy
+from pydantic import BaseSettings
+from pydantic import Field
 
-DATABASE_URL = os.environ['POSTGRES_URL']
+DB_MODELS = ["banana.user"]
+POSTGRES_DB_URL = "postgres://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
 
-database = databases.Database(DATABASE_URL)
-metadata = sqlalchemy.MetaData()
 
-engine = sqlalchemy.create_engine(str(database.url))
-metadata.create_all(engine)
+class PostgresSettings(BaseSettings):
+    postgres_user: str = Field(env="POSTGRES_USER")
+    postgres_password: str = Field(env="POSTGRES_PASSWORD")
+    postgres_db: str = Field(env="POSTGRES_DB")
+    postgres_port: str = Field(env="POSTGRES_PORT")
+    postgres_host: str = Field(env="POSTGRES_HOST")
+
+
+class TortoiseSettings(BaseSettings):
+    db_url: str
+    modules: dict
+    generate_schemas: bool
+
+    @classmethod
+    def generate(cls):
+        postgres = PostgresSettings()
+        db_url = POSTGRES_DB_URL.format(**postgres.dict())
+        del postgres
+        modules = {"models": DB_MODELS}
+        return TortoiseSettings(db_url=db_url, modules=modules, generate_schemas=True)
