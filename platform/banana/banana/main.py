20a1e2e8ae5bd9bd0ca9e40b3d85db412d58ee10
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-10-16 20:06:36 +0300

    Separate file for exception handler

--- platform/banana/banana/main.py
+++ platform/banana/banana/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
-from fastapi import Request
 
-from fastapi.responses import JSONResponse
 from tortoise.contrib.fastapi import register_tortoise
 
 from . import user_api
-from . import resource_api
-from .exceptions import CredentialsException
+from .api import resource_api
+
+from .exceptions import CredentialsException, credentials_exception_handler
 from .postgres_db import TortoiseSettings
 
 
@@ -14,6 +13,7 @@ app = FastAPI()
 app.include_router(user_api.user_router)
 app.include_router(resource_api.resource_router)
 
+app.add_exception_handler(CredentialsException, credentials_exception_handler)
 
 @app.on_event("startup")
 def startup():
@@ -23,13 +23,4 @@ def startup():
         db_url=config.db_url,
         generate_schemas=config.generate_schemas,
         modules=config.modules,
-    )
-
-
-@app.exception_handler(CredentialsException)
-def credentials_exception_handler(_request: Request, _exc: CredentialsException):
-    return JSONResponse(
-        status_code=401,
-        content=dict(message="Could not validate credentials"),
-        headers={"WWW-Authenticate": "Bearer"},
     )
