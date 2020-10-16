4367bb253f9948d34b7e8b5bb523c6e26cd68251
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-10-16 22:04:47 +0300

    Make resource useful

--- platform/banana/banana/api/resource_api.py
+++ platform/banana/banana/api/resource_api.py
@@ -2,14 +2,21 @@ from typing import Dict
 
 import fastapi
 
+from recruiter.utils.service_config import get_info
+
 resource_router = fastapi.APIRouter()
 
 
 @resource_router.get("/")
-async def read_root() -> Dict[str, str]:
-    return {"msg": "Hello, World!"}
+async def get_root() -> Dict[str, str]:
+    return {"message": "OK"}
 
 
-@resource_router.get("/hello")
-async def read_hello() -> Dict[str, str]:
-    return {"msg": "Welcome to Banana World"}
+@resource_router.get("/health")
+async def get_health() -> Dict[str, str]:
+    return {"message": "OK"}
+
+
+@resource_router.get("/version")
+async def get_version() -> Dict[str, str]:
+    return get_info()
--- platform/banana/banana/api/resource_api_test.py
+++ platform/banana/banana/api/resource_api_test.py
@@ -6,12 +6,21 @@ async def test_root(async_api):
     response = await async_api.get('/')
     response.raise_for_status()
     response_json = response.json()
-    assert response_json == {"msg": "Hello, World!"}
+    assert response_json == {"message": "OK"}
 
 
 @pytest.mark.asyncio
-async def test_hello(async_api):
-    response = await async_api.get('/hello')
+async def test_health(async_api):
+    response = await async_api.get('/health')
     response.raise_for_status()
     response_json = response.json()
-    assert response_json == {"msg": "Welcome to Banana World"}
+    assert response_json == {"message": "OK"}
+
+
+@pytest.mark.asyncio
+async def test_version(async_api):
+    response = await async_api.get('/version')
+    response.raise_for_status()
+    response_json = response.json()
+    result = response_json['version'].split(':')[0]
+    assert result == 'recruiter-1912/banana'
