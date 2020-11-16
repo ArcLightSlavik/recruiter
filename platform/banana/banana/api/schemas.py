a3e7977b263e8b6ead5423e1c2c17fdd1ba180b4
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-11-16 22:18:43 +0200

    Models and schemas

---
+++ platform/banana/banana/api/schemas.py
@@ -0,0 +1,38 @@
+from typing import List
+from typing import Optional
+
+import pydantic
+
+
+class ItemBase(pydantic.BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(pydantic.BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
