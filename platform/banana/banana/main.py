446c946f73ce2b40f5665f0623cd538b856a7130
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-11-16 22:18:08 +0200

    Startup alchemy and remove exception handler

--- platform/banana/banana/main.py
+++ platform/banana/banana/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 
+from .api import user_api
 from .api import resource_api
 
-from .exceptions import CredentialsException, credentials_exception_handler
-
+from .postgres_db import alchemy_settings
 
 app = FastAPI()
-app.include_router(resource_api.resource_router, tags=["resource"])
 
-app.add_exception_handler(CredentialsException, credentials_exception_handler)
+app.include_router(user_api.user_router, tags=['user'])
+app.include_router(resource_api.resource_router, tags=['resource'])
+
+
+@app.on_event("startup")
+async def start_db():
+    async with alchemy_settings.get_engine().begin() as conn:
+        await conn.run_sync(alchemy_settings.get_base().metadata.drop_all)
+        await conn.run_sync(alchemy_settings.get_base().metadata.create_all)
