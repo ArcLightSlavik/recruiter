332d014f3956e2abc1fbde982862665707d4ec5c
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-11-16 22:21:38 +0200

    Apis

---
+++ platform/banana/banana/api/user_api.py
@@ -0,0 +1,42 @@
+from typing import List
+
+import fastapi
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from . import crud
+from . import schemas
+from ..postgres_db import get_db
+
+user_router = fastapi.APIRouter()
+
+
+@user_router.post("/users/", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: AsyncSession = fastapi.Depends(get_db)):
+    db_user = await crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise fastapi.HTTPException(status_code=400, detail="Email already registered")
+    return await crud.create_user(db=db, user=user)
+
+
+@user_router.get("/users/", response_model=List[schemas.User])
+async def read_users(skip: int = 0, limit: int = 100, db: AsyncSession = fastapi.Depends(get_db)):
+    return await crud.get_users(db, skip=skip, limit=limit)
+
+
+@user_router.get("/users/{user_id}", response_model=schemas.User)
+async def read_user(user_id: int, db: AsyncSession = fastapi.Depends(get_db)):
+    db_user = await crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise fastapi.HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@user_router.post("/users/{user_id}/items/", response_model=schemas.Item)
+async def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: AsyncSession = fastapi.Depends(get_db)):
+    return await crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@user_router.get("/items/", response_model=List[schemas.Item])
+async def read_items(skip: int = 0, limit: int = 100, db: AsyncSession = fastapi.Depends(get_db)):
+    return await crud.get_items(db, skip=skip, limit=limit)
