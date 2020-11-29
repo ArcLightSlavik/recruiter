ea7f9c94e150597224a01d62f55bc3bfa82afb14
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-11-29 05:07:42 +0200

    Non async way of startup

--- platform/banana/banana/main.py
+++ platform/banana/banana/main.py
@@ -12,7 +12,5 @@ app.include_router(resource_api.resource_router, tags=['resource'])
 
 
 @app.on_event("startup")
-async def start_db():
-    async with alchemy_settings.get_engine().begin() as conn:
-        await conn.run_sync(alchemy_settings.get_base().metadata.drop_all)
-        await conn.run_sync(alchemy_settings.get_base().metadata.create_all)
+def start_db():
+    alchemy_settings.get_base().metadata.create_all(bind=alchemy_settings.get_engine())
