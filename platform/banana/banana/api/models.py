c8362eb55ad447dc4c13d76715497023d6ee02ce
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-12-06 15:26:33 +0200

    Remove item

--- platform/banana/banana/api/models.py
+++ platform/banana/banana/api/models.py
@@ -2,9 +2,6 @@ from sqlalchemy import String
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import Boolean
-from sqlalchemy import ForeignKey
-
-from sqlalchemy.orm import relationship
 
 from ..postgres_db import alchemy_settings
 
@@ -16,16 +13,3 @@ class User(alchemy_settings.get_base()):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
-
-
-class Item(alchemy_settings.get_base()):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
