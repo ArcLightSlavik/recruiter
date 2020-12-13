e406af2220d8b3796ba979c213c1c07a8018c748
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-12-13 21:15:11 +0200

    Remove everything from banana

--- platform/banana/banana/main.py
+++ platform/banana/banana/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
 
-from .api import user_api
 from .api import resource_api
-
-from .postgres_db import alchemy_settings
 
 app = FastAPI()
 
-app.include_router(user_api.user_router, tags=['user'])
 app.include_router(resource_api.resource_router, tags=['resource'])
-
-
-@app.on_event("startup")
-def start_db():
-    alchemy_settings.get_base().metadata.create_all(bind=alchemy_settings.get_engine())
--- platform/banana/banana/postgres_db.py
+++ platform/banana/banana/postgres_db.py
@@ -1,63 +1,63 @@
-from __future__ import annotations
-
-from pydantic import Field
-from pydantic import BaseSettings
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-SQLALCHEMY_DATABASE_URL = "postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
-
-
-class PostgresSettings(BaseSettings):
-    postgres_user: str = Field(env="POSTGRES_USER")
-    postgres_password: str = Field(env="POSTGRES_PASSWORD")
-    postgres_db: str = Field(env="POSTGRES_DB")
-    postgres_port: str = Field(env="POSTGRES_PORT")
-    postgres_host: str = Field(env="POSTGRES_HOST")
-
-
-class AlchemySettings:
-
-    def __init__(self):
-        self._db_url = None
-        self._alchemy_base = None
-        self._alchemy_engine = None
-        self._alchemy_session = None
-
-        self.generate()
-
-    def generate(self) -> None:
-        postgres = PostgresSettings()
-        self._alchemy_base = declarative_base()
-        self._db_url = SQLALCHEMY_DATABASE_URL.format(**postgres.dict())
-        self._alchemy_engine = create_engine(self._db_url)
-        self._alchemy_session = sessionmaker(autocommit=False, autoflush=False, bind=self._alchemy_engine)
-
-    def get_base(self):
-        if self._alchemy_base is None:
-            self.generate()
-        return self._alchemy_base
-
-    def get_engine(self):
-        if self._alchemy_engine is None:
-            self.generate()
-        return self._alchemy_engine
-
-    def get_session(self):
-        if self._alchemy_session is None:
-            self.generate()
-        return self._alchemy_session
-
-
-alchemy_settings = AlchemySettings()
-
-
-# Dependency
-def get_db():
-    db = alchemy_settings.get_session()()
-    try:
-        yield db
-    finally:
-        db.close()
+# from __future__ import annotations
+#
+# from pydantic import Field
+# from pydantic import BaseSettings
+#
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+#
+# SQLALCHEMY_DATABASE_URL = "postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
+#
+#
+# class PostgresSettings(BaseSettings):
+#     postgres_user: str = Field(env="POSTGRES_USER")
+#     postgres_password: str = Field(env="POSTGRES_PASSWORD")
+#     postgres_db: str = Field(env="POSTGRES_DB")
+#     postgres_port: str = Field(env="POSTGRES_PORT")
+#     postgres_host: str = Field(env="POSTGRES_HOST")
+#
+#
+# class AlchemySettings:
+#
+#     def __init__(self):
+#         self._db_url = None
+#         self._alchemy_base = None
+#         self._alchemy_engine = None
+#         self._alchemy_session = None
+#
+#         self.generate()
+#
+#     def generate(self) -> None:
+#         postgres = PostgresSettings()
+#         self._alchemy_base = declarative_base()
+#         self._db_url = SQLALCHEMY_DATABASE_URL.format(**postgres.dict())
+#         self._alchemy_engine = create_engine(self._db_url)
+#         self._alchemy_session = sessionmaker(autocommit=False, autoflush=False, bind=self._alchemy_engine)
+#
+#     def get_base(self):
+#         if self._alchemy_base is None:
+#             self.generate()
+#         return self._alchemy_base
+#
+#     def get_engine(self):
+#         if self._alchemy_engine is None:
+#             self.generate()
+#         return self._alchemy_engine
+#
+#     def get_session(self):
+#         if self._alchemy_session is None:
+#             self.generate()
+#         return self._alchemy_session
+#
+#
+# alchemy_settings = AlchemySettings()
+#
+#
+# # Dependency
+# def get_db():
+#     db = alchemy_settings.get_session()()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
