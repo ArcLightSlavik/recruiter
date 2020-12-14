4f4a23c8532de450fafdc42b5c3e00ec240acee2
ArcLightSlavik <arclight.leskiv@gmail.com>
2020-12-14 21:02:11 +0200

    Add cors and company irl

--- platform/suzuka/suzuka/main.py
+++ platform/suzuka/suzuka/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from .api import company_api
 from .api import resource_api
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:6622",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(company_api.company_router, tags=['company'])
 app.include_router(resource_api.resource_router, tags=['resource'])
